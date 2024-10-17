c8dfd92b912ec08d0993a04fd55fc4c4a4efcc0e
Raunak Agarwal <71973645+Raunak173@users.noreply.github.com>
2024-10-17 23:37:06 +0530

    change in connection string

--- initializers/database.go
+++ initializers/database.go
@@ -13,7 +13,8 @@ var Db *gorm.DB
 func ConnectToDB() {
 	var err error
 	dsn := os.Getenv("DB_URL")
-	Db, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	// Db, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	Db, err = gorm.Open(postgres.Open(dsn))
 	if err != nil {
 		log.Fatal("Error in connectin to DB")
 	}
--- main.go
+++ main.go
@@ -8,7 +8,8 @@ import (
 )
 
 func init() {
-	// initializers.LoadEnv()  Cant use this in deployment
+	// Cant use this in deployment
+	// initializers.LoadEnv()
 	initializers.ConnectToDB()
 	initializers.SyncDB()
 }
